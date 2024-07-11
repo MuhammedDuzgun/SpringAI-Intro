b3b222a7dfc2ea2c964dff48e0538debf6fa3e14
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-07-12 02:53:25 +0300

    Modify - TestController

--- src/main/java/com/spring_ai/intro/controller/TestController.java
+++ src/main/java/com/spring_ai/intro/controller/TestController.java
@@ -1,10 +1,12 @@
 package com.spring_ai.intro.controller;
 
+import com.spring_ai.intro.output.MusicArtist;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/api/ai")
@@ -13,16 +15,36 @@ public class TestController {
     private final ChatClient chatClient;
 
     public TestController(ChatClient.Builder chatClientBuilder) {
-        this.chatClient = chatClientBuilder.build();
+        this.chatClient = chatClientBuilder
+                .defaultSystem("Kullanıcıdan gelen müzik sanatçısına benzer sanatçıları json listesi olarak" +
+                        "sıralayan bir asistansın.")
+                .build();
     }
 
     @GetMapping
-    String generation(@RequestBody String userInput) {
+    public String generation(@RequestBody String userInput) {
         String response = chatClient.prompt()
                 .user(userInput)
                 .call()
                 .content();
         return response;
+    }
+
+    @GetMapping("/stream")
+    public Flux<String> stream(@RequestBody String userInput) {
+        return chatClient.prompt()
+                .user(userInput)
+                .stream()
+                .content();
+    }
+
+    @GetMapping("/music-artist")
+    public MusicArtist getSimilarArtist(@RequestBody String artist) {
+        MusicArtist musicArtist = chatClient.prompt()
+                .user(artist)
+                .call()
+                .entity(MusicArtist.class);
+        return musicArtist;
     }
 
 }
